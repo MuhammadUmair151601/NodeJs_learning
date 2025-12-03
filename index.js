601dcc0ab694706cfdb668fa6c4a5f57b645b940
MuhammadUmair151601 <157686720+MuhammadUmair151601@users.noreply.github.com>
2025-12-03 16:42:14 +0500

    app.response done

---
+++ index.js
@@ -0,0 +1,24 @@
+// const express = require('express');
+// const app = express();
+
+// const PORT = 3000;
+
+// app.get('/', (req, res) => {
+//   res.send('<h1>Hello everyone</h1>');
+// });
+
+// app.listen(PORT, () => {
+//   console.log(`Port ${PORT} connected successfully`);
+// });
+
+// const express = require('express');
+// const app = express();
+
+// const PORT = 3001;
+// app.listen(PORT,()=>{
+//   console.log(`port ${PORT} connected successfully.`)
+// })
+
+// app.get('/', (req,res)=>{
+//   res.send("<h1>HELLO everyone</h1>")
+// })
