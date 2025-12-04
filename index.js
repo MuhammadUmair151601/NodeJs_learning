144eb7a7ed88b7ac83abb5fb6e5df04db255e8a1
MuhammadUmair151601 <157686720+MuhammadUmair151601@users.noreply.github.com>
2025-12-04 18:09:02 +0500

    respose.sendfile done

--- index.js
+++ index.js
@@ -101,3 +101,18 @@
 // app.get('/downloads',(req,res)=>{
 //   res.download('./downloads/layers.png')
 // })
+
+// ****************sendfile is also same as download but it gives you option to download:
+const express = require('express');
+const app = express();
+const PORT = 3001;
+
+app.set('./downloads/layers')
+
+app.listen(PORT,()=>{
+  console.log(`port ${PORT} successfully connected:`)
+});
+
+app.get('/downloads',(req,res)=>{
+  res.sendFile(__dirname + '/downloads/layers.png')
+})
