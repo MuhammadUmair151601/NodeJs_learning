a9cf909a91f87fa70bba352ec245a6543e911093
MuhammadUmair151601 <157686720+MuhammadUmair151601@users.noreply.github.com>
2025-12-17 17:42:42 +0500

    rm and unlink methods done:

--- index.js
+++ index.js
@@ -25,3 +25,21 @@
 // })
 
 // this will copy all the text from file2 and paste it in the hello.txt
+
+// now fs.remdir and fs.unlink are used for deletion but remdir for directory unlink for deleting file
+
+// const fs = require('fs');
+
+// fs.rm("C:/Users/PMLS/Desktop/NodeJS learning/directory", {
+//   recursive: true,
+//   force: true
+// }, (err) => {
+//   if (err) console.error(err);
+//   else console.log("done");
+// });
+
+// const fs = require('fs')
+// fs.unlink("./Hello.txt", (err)=>{
+//   if(err) console.error(err)
+//     else console.log("done")
+// })
