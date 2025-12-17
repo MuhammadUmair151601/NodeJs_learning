f03d54d6c03e1b05943bdab74326c44c27b4b6b5
MuhammadUmair151601 <157686720+MuhammadUmair151601@users.noreply.github.com>
2025-12-17 17:05:11 +0500

    writeFile,appenFile and rename done

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
-const fs = require ('fs')
-fs.writeFile("Hi.txt","Hello everyone how are you all", function(err){
-  if(err)console.error(err)
-    else console.log("done")
-})
+// const fs = require ('fs')
+// fs.writeFile("Hi.txt","Hello everyone how are you all", function(err){
+//   if(err)console.error(err)
+//     else console.log("done")
+// })
+
+// const fs = require('fs')
+// fs.appendFile("file2","Hello all of you are selected", function(err){
+//   if(err)console.error(err)
+//     else console.log("done")
+// })
+
+// appendFile is same as writeFile:
+
+// const fs = require('fs')
+// fs.rename("Hi.txt","Hello.txt", (err)=>{
+//   if(err) console.error(err)
+//     else console.log("done")
+// })
+
