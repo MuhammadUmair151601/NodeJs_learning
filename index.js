2a37b182cbf4512778b00f5b330bc9b86cf0996f
MuhammadUmair151601 <157686720+MuhammadUmair151601@users.noreply.github.com>
2025-12-17 17:29:43 +0500

    fs.copyFile done

--- index.js
+++ index.js
@@ -18,3 +18,10 @@
 //     else console.log("done")
 // })
 
+// const fs = require ('fs')
+// fs.copyFile("file2","Hello.txt",(err)=>{
+//   if(err) console.error(err)
+//     else console.log("done")
+// })
+
+// this will copy all the text from file2 and paste it in the hello.txt
