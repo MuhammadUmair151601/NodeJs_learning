d04ce6841379e7833435e585880ab3a47260e94c
MuhammadUmair151601 <157686720+MuhammadUmair151601@users.noreply.github.com>
2025-12-25 15:19:26 +0500

    Ejs connected to public files and also public file routing set

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require ('express')
 const app = express();
+const path = require('path')
 
 // For taking forms
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
+
+// we will go to public folder for finding static folders like stylesheets images and js files:
+app.use(express.static(path.join(__dirname, 'public')))
 
 // setup ejs for view engine as middleware:
 app.set('view engine', 'ejs');
