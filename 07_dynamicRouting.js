4b6c502f148734649fa5a7bf3a7c6f6e3c4e0791
MuhammadUmair151601 <157686720+MuhammadUmair151601@users.noreply.github.com>
2025-12-30 09:13:25 +0500

    creating first sample project

---
+++ 07_dynamicRouting.js
@@ -0,0 +1,22 @@
+// // Dynamic routing in nodeJS:
+
+// const express = require ('express');
+// const app = express()
+
+// app.use(express.json())
+// app.use(express.urlencoded({extended: true}))
+// // app.use(express.static(path.join(__dirname, 'public')))
+
+// app.set('view engine', 'ejs')
+
+// app.get('/about' ,(req,res)=>{
+//   res.send("chal gaya")
+// })
+// app.get('/username/:id', (req,res)=>{
+//   res.send(`welcome to roll number ${req.params.id} in new batch:`)
+// })
+
+// port = 3000;
+// app.listen(port, (req,res)=>{
+//   console.log("server connected to port 3000:")
+// })
--- index.js
+++ index.js
@@ -1,22 +1,17 @@
-// Dynamic routing in nodeJS:
-
-const express = require ('express');
-const app = express()
-
-app.use(express.json())
-app.use(express.urlencoded({extended: true}))
-// app.use(express.static(path.join(__dirname, 'public')))
+const express = require ('express')
+const app = express();
 
 app.set('view engine', 'ejs')
 
-app.get('/about' ,(req,res)=>{
-  res.send("chal gaya")
-})
-app.get('/username/:id', (req,res)=>{
-  res.send(`welcome to roll number ${req.params.id} in new batch:`)
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+// app.use(express.static(path.join(__dirname,"public")))
+
+app.get('/',(req,res)=>{
+  res.send("Hello everyone")
 })
 
-port = 3000;
-app.listen(port, (req,res)=>{
-  console.log("server connected to port 3000:")
+const port = 3000;
+app.listen(port,(req,res)=>{
+  console.log("server connected successfully:")
 })
