2f4ef1484f64b554a6f5c0e2bf438e0a149b6dd4
MuhammadUmair151601 <157686720+MuhammadUmair151601@users.noreply.github.com>
2026-01-06 12:00:00 +0500

    lect 3 revision done

--- index.js
+++ index.js
@@ -1,22 +1,53 @@
-const express = require ('express')
-const app = express(); 
-const path = require('path')
-const fs = require('fs')
+// const express = require ('express')
+// const app = express(); 
+// const path = require('path')
+// const fs = require('fs')
 
-app.set('view engine', 'ejs')
+const { join } = require('path');
 
-app.use(express.json())
-app.use(express.urlencoded({extended: true}))
-// app.use(express.static(path.join(__dirname,"public")))
+// app.set('view engine', 'ejs')
 
-app.get('/',(req,res)=>{
-  fs.readdir(`./files`, (err,files)=>{
-    res.render("index",{files: files});
-  })
-  res.render("index")
-})
+// app.use(express.json())
+// app.use(express.urlencoded({extended: true}))
+// // app.use(express.static(path.join(__dirname,"public")))
 
-const port = 3000;
-app.listen(port,(req,res)=>{
-  console.log("server connected successfully:")
-})
+// app.get('/',(req,res)=>{
+//   fs.readdir(`./files`, (err,files)=>{
+//     res.render("index",{files: files});
+//   })
+//   res.render("index")
+// })
+
+// const port = 3000;
+// app.listen(port,(req,res)=>{
+//   console.log("server connected successfully:")
+// })
+
+// const fs = require ('fs')
+// const filename = "hello.txt"
+// const content = "hi everyone"
+// fs.writeFile(filename, content,(err)=>{
+// if(err){
+//   console.log(`error writting ${filename}`)
+// }else{
+//   console.log(`successfully created ${filename}`)
+// }
+// })
+// fs.AppendFile same as write file.
+// const fs = require ('fs')
+// fs.rename("hello.txt","hi.txt", (err)=>{
+//   if(err) console.log(err)
+//     else console.log("done")
+// })
+
+// const fs = require('fs')
+// fs.copyFile("hi.txt","javascripts",(err)=>{
+//   if(err) console.log(err)
+//     else console.log("done")
+// })
+
+// const fs = require('fs')
+// fs.unlink("./javascripts", (err)=>{
+//   if(err) console.log(err)
+//     else("done")
+// })
--- revision.js
+++ revision.js
@@ -1,1 +1,8 @@
 // lecture 1 js basics revision done"
+// let arr = [1,2,3,4,5,6,7,8,9]
+
+// let newarr = arr.forEach((val)=>{
+//   return val*3
+// })
+
+// console.log(newarr);
